2f61c96afec1b75a90e84d22e9006b2878b8f9f9
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2023-11-23 13:14:32 -0500

    Added title screen which moves to main world when space is pressed

---
+++ TitleScreen.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The title screen which introduces the player to the game
+ * 
+ * @author Darren
+ * @version Nov 2023
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Elephant", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
+    }
+    
+    //Main world's act loop
+    
+    public void act(){
+        //Start game once player presses space bar
+        if(Greenfoot.isKeyDown("space")){
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+}
